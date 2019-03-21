4ccd82265acf6726c8cb907e396b4f58c260c6b9
valujko <valujko@gmail.com>
2019-03-21 19:43:11 +0300

    1 task

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmptyArray();
+            Console.ReadKey();
+        }
+        static void EmptyArray()
+        {
+            int[] emptyArray = new int[0];
+            Console.Write("Length of array = :");
+            Console.WriteLine(emptyArray.Length);
+        }
+
+
+       
+    }
+
+}
