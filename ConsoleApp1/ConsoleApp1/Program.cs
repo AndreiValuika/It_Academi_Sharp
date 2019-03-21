0eb5ee300c57612cbd3eb4d86f39f73b03e7a5b1
valujko <valujko@gmail.com>
2019-03-21 19:52:07 +0300

    All

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,6 +15,9 @@ namespace ConsoleApp1
 
             ArrayOF3();
             Console.ReadKey();
+
+            RandomArray();
+            Console.ReadKey();
         }
 
         static void EmptyArray()
@@ -31,13 +34,32 @@ namespace ConsoleApp1
                 Console.WriteLine(o);
 
             objArray[0] = (int)objArray[0] + 10;
-            objArray[2] = (String)objArray[0] + ", guys!";
+            objArray[2] = (String)objArray[2] + ", guys!";
 
             foreach (object o in objArray)
                 Console.WriteLine(o);
         }
 
-      //  void RandomArray()
+        static void RandomArray()
+        {
+            Random rnd = new Random();
+            int[] tempArray =new int[13]; 
+
+
+            for (int i = 0; i < 13; i++)
+            {
+                tempArray[i] = rnd.Next(100);
+            }
+
+            int max = tempArray[0];
+
+            foreach (int i in tempArray)
+                if (max < i) max = i;
+
+            foreach (int i in tempArray)
+                Console.WriteLine(i);
+            Console.WriteLine($"max={max}");
+        }
     }
 
 }
