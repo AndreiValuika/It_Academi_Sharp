3e25efddd298e851ffcd19dfdd9f83590599b5e8
AndreiValuika <48656054+AndreiValuika@users.noreply.github.com>
2019-04-16 20:27:21 +0300

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int startCount = Convert.ToInt32(Console.ReadLine());
+
+            UniqueItem.id = startCount;
+
+            for (int i = 0; i < 10; i++)
+            {
+                new UniqueItem();
+            }
+
+            Console.WriteLine(UniqueItem.id);
+
+            Console.ReadKey();
+
+        }
+    }
+
+   class UniqueItem
+    {
+        public static int id { get; set; }
+
+        public UniqueItem()
+        {
+            id++;
+        }
+    }
+}
