a7eb9670c798e51cfe32b8ee1832e846e6071019
Valujko <Valujko@gmail.com>
2019-04-16 17:05:28 +0300

    Shape-Triangle-Circle

---
+++ hm_10/Figure.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Square:Shape
+{
+	public Square()
+	{
+	}
+
+    public Square(int length) : base(length)
+    {
+    }
+
+    public override double GetArea()
+    {
+        return length*length;
+    }
+}
+
+public class Triangle : Shape
+{
+    public Triangle()
+    {
+    }
+
+    public Triangle(int length) : base(length)
+    {
+    }
+
+    public override double GetArea()
+    {
+        return Math.Sqrt(3)/4 * length*length;
+    }
+}
+public class Circle : Shape
+{
+    public Circle()
+    {
+    }
+
+    public Circle(int length) : base(length)
+    {
+    }
+    public override double GetArea()
+    {
+        return Math.PI*length*length;
+    }
+}
