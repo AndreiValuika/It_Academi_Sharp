a4b522c7c190f33f78534b803ce37e3b479eeddd
AndreiValuika <48656054+AndreiValuika@users.noreply.github.com>
2019-04-23 20:37:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lc_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string:");
+            String inputString;
+            inputString = Console.ReadLine();
+            Console.WriteLine(inputString.Five());
+            Console.ReadKey();
+        }
+    }
+    static class StringExtension
+    {
+        public static string Five(this String str)
+        {
+            return str.Substring(0, 5) + "...";
+        }
+    }
+}
