f4d2c05a7bb1ad38910669d76b8938dc3c64c002
Valujko <Valujko@gmail.com>
2019-04-24 09:45:25 +0300

    StackExamle(Object)

---
+++ StackExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackEx stackEx = new StackEx();
+            stackEx.push(1);
+            stackEx.push("Intel");
+            stackEx.push(true);
+
+            Console.WriteLine(stackEx.pop());
+            Console.WriteLine(stackEx.pop());
+            Console.WriteLine(stackEx.pop());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ StackExample/StackEx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExample
+{
+    class StackEx
+    {
+        Node top;
+        public Object pop()
+        {
+            if (top!=null)
+            {
+                Object result = top.Body;
+                top = top.Next;
+                return result;
+            }
+           
+            throw new Exception("Stack is empty.");
+        }
+
+        public void push(Object body)
+        {
+            if (top==null)
+            {
+                top = new Node();
+                top.Body = body;
+                top.Next = null;
+            }
+            else
+            {
+                Node tempNode = new Node();
+                tempNode.Body = body;
+                tempNode.Next = top;
+                top = tempNode;
+            }
+        }
+    }
+
+}
